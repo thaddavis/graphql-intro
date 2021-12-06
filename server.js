18e8763dd74888ffcc2437e49e8bcd06159fd5e3
Tad <a@as-MacBook-Pro.local>
2021-12-06 13:33:43 -0500

    save project

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test');
 
-// var schema = require('./schemaComposers')
+var schema = require('./schemaComposers')
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
 
 var root = { hello: () => 'Hello world!!!' };
 
